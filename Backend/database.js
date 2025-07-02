779fa62bd88bb49c16389309acace8271ee6571b
Adewale Ogen <ogenadewalex@gmail.com>
2025-07-02 01:24:21 +0100

    deployment 1.1

--- Backend/database.js
+++ Backend/database.js
@@ -1,14 +1,5 @@
 import mysql from "mysql2/promise";
 
-// Create a connection pool
-const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "hurleybaba",
-  database: "votedb",
-  waitForConnections: true,
-  connectionLimit: 10, // max number of connections
-  queueLimit: 0, // unlimited queued requests
-});
+const pool = mysql.createPool(process.env.DATABASE_URL);
 
 export default pool;
