e48b5e4dd6e2cec66dcb80b53acc87f4ad8d5fe0
Adewale Ogen <ogenadewalex@gmail.com>
2025-07-02 01:38:14 +0100

    deployment 3.0 frontend

--- Vote_app/app/baseUrl.js
+++ Vote_app/app/baseUrl.js
@@ -1,4 +1,3 @@
+// export const baseUrl = "http://192.168.43.157:3000"; // For physical device
 
-export const baseUrl = "http://192.168.43.157:3000"; // For physical device
-
-
+export const baseUrl = "https://ielectapp.onrender.com";
